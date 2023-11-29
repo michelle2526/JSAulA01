df5cbfa5f98acd0f29dc6993a6137a488408c643
michelle2526 <90350891+michelle2526@users.noreply.github.com>
2023-11-29 11:51:17 -0300

    AULA de Ecmascript
    comando ecmascript

--- Aula-6/atividade/script.js
+++ Aula-6/atividade/script.js
@@ -1,45 +1,101 @@
-function cadastrarFilme() {
-    // Obter valores do formulário
-    var nome = document.getElementById("nome").value;
-    var descricao = document.getElementById("descricao").value;
-    var data = document.getElementById("data").value;
-    var diretor = document.getElementById("diretor").value;
-    var categoria = document.getElementById("categoria").value;
+// function cadastrarFilme() {
+//     // Obter valores do formulário
+//     var nome = document.getElementById("nome").value;
+//     var descricao = document.getElementById("descricao").value;
+//     var data = document.getElementById("data").value;
+//     var diretor = document.getElementById("diretor").value;
+//     var categoria = document.getElementById("categoria").value;
 
-    // Validar se os campos foram preenchidos
-    if (nome && descricao && data && diretor && categoria) {
-        // Criar objeto de filme
-        var filme = {
-            nome: nome,
-            descricao: descricao,
-            data: data,
-            diretor: diretor,
-            categoria: categoria
-        };
+//     // Validar se os campos foram preenchidos
+//     if (nome && descricao && data && diretor && categoria) {
+//         // Criar objeto de filme
+//         var filme = {
+//             nome: nome,
+//             descricao: descricao,
+//             data: data,
+//             diretor: diretor,
+//             categoria: categoria
+//         };
 
-        // Adicionar filme à lista
-        adicionarFilmeNaLista(filme);
+//         // Adicionar filme à lista
+//         adicionarFilmeNaLista(filme);
 
-        // Limpar o formulário
-        document.getElementById("formularioFilme").reset();
-    } else {
-        alert("Por favor, preencha todos os campos.");
+//         // Limpar o formulário
+//         document.getElementById("formularioFilme").reset();
+//     } else {
+//         alert("Por favor, preencha todos os campos.");
+//     }
+// }
+
+// function adicionarFilmeNaLista(filme) {
+//     // Obter a lista de filmes
+//     var filmeList = document.getElementById("filmeList");
+
+//     // Criar um novo item de filme
+//     var novoFilmeItem = document.createElement("li");
+//     novoFilmeItem.className = "filmeItem";
+//     novoFilmeItem.innerHTML = "<strong>Nome:</strong> " + filme.nome +
+//         "<br><strong>Descrição:</strong> " + filme.descricao +
+//         "<br><strong>Data de Lançamento:</strong> " + filme.data +
+//         "<br><strong>Diretor:</strong> " + filme.diretor +
+//         "<br><strong>Categoria:</strong> " + filme.categoria;
+
+//     // Adicionar o novo item à lista
+//     filmeList.appendChild(novoFilmeItem);
+// }
+
+class Filmes {
+    constructor(valNome,valDescricao,valData,ValDiretor,valCategoria){
+        this.nome = valNome;
+        this.descricao = valDescricao;
+        this.data = valData;
+        this.diretor = ValDiretor;
+        this.categoria = valCategoria;
     }
 }
 
-function adicionarFilmeNaLista(filme) {
-    // Obter a lista de filmes
-    var filmeList = document.getElementById("filmeList");
+// seletores
 
-    // Criar um novo item de filme
-    var novoFilmeItem = document.createElement("li");
-    novoFilmeItem.className = "filmeItem";
-    novoFilmeItem.innerHTML = "<strong>Nome:</strong> " + filme.nome +
-        "<br><strong>Descrição:</strong> " + filme.descricao +
-        "<br><strong>Data de Lançamento:</strong> " + filme.data +
-        "<br><strong>Diretor:</strong> " + filme.diretor +
-        "<br><strong>Categoria:</strong> " + filme.categoria;
+const btnCadastrar = document.querySelector("#cadastrarFilmesbtn");
+const btnlistar = document.querySelector("#listarFilmesbtn");
+var filmes = []
 
-    // Adicionar o novo item à lista
-    filmeList.appendChild(novoFilmeItem);
+
+btnCadastrar.addEventListener("click",cadastrar);
+btnlistar.addEventListener("click",listar);     
+
+
+function cadastrar (){
+    let nome =document.querySelector("#nome").value;;
+    let descricao = document.querySelector("#descricao").value;
+    let data = document.querySelector("#data").value;
+    let diretor = document.querySelector("#diretor").value;
+    let categoria = document.querySelector("#categoria").value;
+
+
+ filmes.push = (new Filmes (nome,descricao,data,diretor,categoria))
+ 
+
+    
+    
+    
 }
+
+function listar(){
+    let divresposta = document.querySelector("#resposta");
+
+
+     filmes.forEach((elementos) =>{
+        divresposta.innerHTML += `<div class="Filme">
+        <h2> ${elementos.nome}</h2>
+        <p> Descriçao do filme ; ${elementos.descricao}</p>
+        <p> data de lancamento : ${elementos.data}</p>
+        <p> diretor do filme : ${elementos.diretor}</p>
+        <p> categoria do filme : ${elementos.categoria}</p>
+        </div>
+        `;
+
+     }
+     );
+ }
+    
---
+++ Aula-7 e 8 -ECMASCRIPT/MaterialDeAula/JS/script.js
@@ -0,0 +1,68 @@
+// replace / replaceall
+
+var frase = " Ola mundo, o mundo e muito legal";
+console.log(frase);
+
+console.log(frase.replace("mundo","bacana"));
+console.log(frase.replaceAll("mundo","bacana"));
+
+
+//SETATRIBUTO
+
+ document.querySelector("span").setAttribute("style","color: blue;");
+
+// // CLASS LIST --> para gerenciar a classe
+
+
+// // document.querySelector("#classlist").classList.remove("claro"); // remove o primeiro item
+// // document.querySelector("#classlist").classList.add("claro"); // adiciona o item
+
+
+//  var elemento = document.querySelector("#classlist");
+//  if(elemento.classList.contains("claro")){ // contains se contem a classe
+//      //elemento.classList.toggle("escuro"); // toggle troca um para o outro
+//      elemento.classList.replace("claro","escuro"); // replace subistue o primeiro pelo segundo
+//  }
+
+ //INDEX OF
+
+ var FRUTAS = ['Morango','Banana','Laranja','Uva']
+
+ console.log(FRUTAS.includes("Morango")); // includes thue e false
+ console.log(FRUTAS.indexOf("Morango")); // index of vlor do index do array(caso nao exista,-1)
+
+ if(FRUTAS.indexOf("Abacaxi"== 1)){
+     console.log("nao achei")
+ }
+
+
+
+ // SET TIMEOUT
+
+ function msg(){
+    return new Promise ((teste) => {
+        setTimeout(() => {
+            console.log("seja Bem-vindo!");
+        },2000);
+        teste()
+        
+    } )
+   
+    
+ }
+
+  async function carregando(){
+      console.log("iniciou");
+      await msg();
+      console.log("Bom dia!");
+
+
+
+
+}
+
+carregando();
+
+ 
+
+
